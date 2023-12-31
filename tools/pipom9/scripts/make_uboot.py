0011c9e509d953033278611f3a560c2485bcc2b2
DmitryMurashov <dmtr.murashov@gmail.com>
2023-12-31 15:13:38 +0300

    [tools:*] Build | Build u-boot

--- tools/pipom9/scripts/make_uboot.py
+++ tools/pipom9/scripts/make_uboot.py
@@ -9,15 +9,46 @@ import tired.logging
 
 
 HERE = pathlib.Path(__file__).resolve().parent
-RKBIN = HERE.parent / "rkbin"
 UBOOT = HERE.parent / "u-boot"
+
+def build_rockchip_uboot():
+    # Change toolchain location
+    cross_compile_command_argument = f"CROSS_COMPILE={os.getenv('CROSS_COMPILE')}"
+
+    # Specify target
+    target_command_argument = "rk3188"
+
+    # Execute
+    tired.logging.info(f"Changing PWD: ${UBOOT}")
+    os.chdir(UBOOT)
+    command = f"make.sh {cross_compile_command_argument} {target_command_argument}"
+    tired.command.execute(command)
+
+
+def clone_radxa_uboot():
+    # Clone from git, if haven't yet
+    if not UBOOT.is_dir():
+        tired.command.execute(f"git clone -b u-boot-rk3188-sdcard https://github.com/radxa/u-boot-rockchip.git {UBOOT}")
+    else:
+        tired.logging.info(f"{UBOOT.name} repository is already present, skipping")
+
+
+def build_radxa_uboot():
+    tired.logging.info(f"Changing PWD: ${UBOOT}")
+    os.chdir(UBOOT)
+    tired.command.execute(f"make rk30xx")
+    tired.command.execute(f"./pack-sd.sh")
 
 
 def main():
-    if not UBOOT.is_dir():
-        tired.command.execute(f"git clone https://github.com/rockchip-linux/u-boot.git {UBOOT}")
-    else:
-        tired.logging.info(f"{UBOOT.name} repository is already present, skipping")
+    # Clone
+    clone_radxa_uboot()
+
+    # Update necessary environment variables
+    rockchip_environment.update_env()
+
+    # Execute
+    build_radxa_uboot()
 
 
 if __name__ == "__main__":
--- tools/pipom9/scripts/rockchip_environment.py
+++ tools/pipom9/scripts/rockchip_environment.py
@@ -11,19 +11,27 @@ HERE = pathlib.Path(__file__).resolve().parent
 IS_PATH_UPDATED = False
 LINARO_TOOLCHAIN = (HERE.parent / "linaro_toolchain" / "arm-cortex_a9-linux-gnueabihf-linaro_4.9.3-2014.12").resolve()
 SHELL_EXECUTABLE = "bash"
+SHOULD_SET_NPROC = True
+
+
+def get_nproc():
+    return 20
+
+
+def setenv(variable, value):
+    tired.logging.info(f'Setting environment variable "{variable}"="{value}"')
+    os.environ[str(variable)] = str(value)
 
 
 def update_env():
-    tired.logging.info("setting environment variable ARCH")
-    os.environ["ARCH"] = "arm"
-    tired.logging.info("setting environment variable SRCARCH")
-    os.environ["SRCARCH"] = "arm"
-    tired.logging.info("setting environment variable INSTALL_MOD_PATH")
-    os.environ["INSTALL_MOD_PATH"] = "./modules"
-    tired.logging.info("setting environment variable INSTALL_PATH")
-    os.environ["INSTALL_PATH"] = "./install"
-    tired.logging.info("setting environment variable PATH")
-    os.environ["PATH"] = os.environ["PATH"] + ":" + str(LINARO_TOOLCHAIN)
+    setenv("ARCH", "arm")
+    setenv("SRCARCH", "arm")
+    setenv("INSTALL_MOD_PATH", "./modules")
+    setenv("INSTALL_PATH", "./install")
+    setenv("PATH", os.environ["PATH"] + ":" + str(LINARO_TOOLCHAIN))
+
+    if SHOULD_SET_NPROC:
+        setenv("MAKEFLAGS", get_nproc())
 
     if SHELL_EXECUTABLE == "bash":
         os.environ["PS1"]
@@ -31,11 +39,9 @@ def update_env():
     should_use_ccache = os.getenv("USE_CCACHE", None)
 
     if should_use_ccache:
-        tired.logging.info("setting environment variable CROSS_COMPILE, using ccache")
-        os.environ["CROSS_COMPILE"] = f"ccache {LINARO_TOOLCHAIN}/bin/arm-eabi-"
+        setenv("CROSS_COMPILE", f"ccache {LINARO_TOOLCHAIN}/bin/arm-eabi-")
     else:
-        tired.logging.info("setting environment variable CROSS_COMPILE, NOT using ccache")
-        os.environ["CROSS_COMPILE"] = f"{LINARO_TOOLCHAIN}/bin/arm-eabi-"
+        setenv("CROSS_COMPILE", f"{LINARO_TOOLCHAIN}/bin/arm-eabi-")
 
 
 if __name__ == "__main__":
