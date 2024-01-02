c5b1929498d1d3f3dd197d9a59c48a5b643f7bdd
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-02 18:09:17 +0300

    [tools:make_pipom9_zimage] Build |

---
+++ tools/pipom9/scripts/make_pipom9_zimage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ZIMAGE_PATH="./../../../arch/arm/boot/zImage"
+DATE=$(date +%Y%m%d%H%M | tr -d \\n)
+
+./../mkbootimg --kernel ${ZIMAGE_PATH} --ramdisk initrd.img --output str90-image-${DATE}.bin
