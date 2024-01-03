66d66ca96c437b636eb87d73064f86d024620c9a
DmitryMurashov <dmtr.murashov@gmail.com>
2024-01-03 17:33:46 +0300

    [tools:rockchip_environment] Build | Kitkat release for Radxa U-Boot

--- tools/pipom9/scripts/rockchip_environment.py
+++ tools/pipom9/scripts/rockchip_environment.py
@@ -5,6 +5,7 @@ import pathlib
 import tired.command
 import tired.fs
 import tired.logging
+import tired.ui
 
 
 HERE = pathlib.Path(__file__).resolve().parent
@@ -12,6 +13,7 @@ IS_PATH_UPDATED = False
 LINARO_TOOLCHAIN = (HERE.parent / "linaro_toolchain" / "arm-cortex_a9-linux-gnueabihf-linaro_4.9.3-2014.12").resolve()
 SHELL_EXECUTABLE = "bash"
 SHOULD_SET_NPROC = True
+RADXA_RECOMMENDED_UBOOT_TOOLCHAIN = (HERE.parent / "toolchain-arm-eabi-4.6")
 
 
 def get_nproc():
@@ -34,7 +36,7 @@ def update_env():
         setenv("MAKEFLAGS", get_nproc())
 
     if SHELL_EXECUTABLE == "bash":
-        os.environ["PS1"]
+        os.environ["PS1"] = "(rockchip environment) " + os.environ["PS1"]
 
     should_use_ccache = os.getenv("USE_CCACHE", None)
 
@@ -44,8 +46,39 @@ def update_env():
         setenv("CROSS_COMPILE", f"{LINARO_TOOLCHAIN}/bin/arm-eabi-")
 
 
+def update_env_uboot():
+    """
+    The upstream U-Boot does not support RK3188. Rockchip recommends using
+    Radxa U-Boot https://wiki.radxa.com/Rock/U-Boot. Radxa U-Boot recommends
+    using a particular version of GCC toolchain.
+    """
+    tired.logging.info(f'Setting up uboot env with "{RADXA_RECOMMENDED_UBOOT_TOOLCHAIN}" toolchain')
+    setenv("ARCH", "arm")
+    setenv("SRCARCH", "arm")
+    setenv("INSTALL_MOD_PATH", "./modules")
+    setenv("INSTALL_PATH", "./install")
+    setenv("PATH", os.environ["PATH"] + ":" + str(RADXA_RECOMMENDED_UBOOT_TOOLCHAIN))
+
+    if SHOULD_SET_NPROC:
+        setenv("MAKEFLAGS", get_nproc())
+
+    if SHELL_EXECUTABLE == "bash":
+        os.environ["PS1"] = "(radxa rockchip environment) " + os.environ["PS1"]
+
+    should_use_ccache = os.getenv("USE_CCACHE", None)
+
+    if should_use_ccache:
+        setenv("CROSS_COMPILE", f"ccache {RADXA_RECOMMENDED_UBOOT_TOOLCHAIN}/bin/arm-eabi-")
+    else:
+        setenv("CROSS_COMPILE", f"{RADXA_RECOMMENDED_UBOOT_TOOLCHAIN}/bin/arm-eabi-")
+
+
 if __name__ == "__main__":
-    update_env()
+    profiles = {
+        "Linaro 4.9": update_env,
+        "GCC 4.6 (Radxa U-Boot-compatible)": update_env_uboot,
+    }
+    tired.ui.select_callback(profiles, "Select profile")
     tired.logging.info("Entering virtual environment")
     os.system(SHELL_EXECUTABLE)
     tired.logging.info("Exited virtual environment")
